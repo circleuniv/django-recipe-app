e033060832184faba4ba55e86f764760cea725ba
circleuniv <circleuniv@users.noreply.github.com>
2025-01-06 23:18:10 +0800

    Under normal circumstances, the data table structure cannot be changed, and the modification of the model field

--- recipe_app/models.py
+++ recipe_app/models.py
@@ -141,7 +141,7 @@ class MenuItems(models.Model):
     description = models.CharField(max_length=2000, blank=True, null=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     meal_type = models.CharField(max_length=200, choices=MEAL_TYPE)
-    author = models.ForeignKey(User, on_delete=models.PROTECT)
+    author = models.ForeignKey(User, on_delete=models.PROTECT,db_column='author')
     status = models.IntegerField(choices=STATUS, default=1)
     date_created = models.DateTimeField(auto_now_add=True)
     date_updated = models.DateTimeField(auto_now=True)
